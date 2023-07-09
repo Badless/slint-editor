af49e130e043d3d77664fa551a5974b3bfa5a019
DevBrainless <79766539+Badless@users.noreply.github.com>
2023-07-09 22:24:38 +0200

    removed not needed lines

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,11 @@
 use std::fs::File;
 use std::io::prelude::Write;
 use std::process::Command;
-use syntect::easy::HighlightLines;
-use syntect::highlighting::{Style, ThemeSet};
-use syntect::parsing::SyntaxSet;
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
     let ui = AppWindow::new()?;
-
-    let ps = SyntaxSet::load_defaults_newlines();
-    let ts = ThemeSet::load_defaults();
-
-    let syntax = ps.find_syntax_by_extension("py").unwrap();
-    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-    let s = "print(\"Hello World!\")";
-    for line in LinesWithEndings::from(s) {
-        let ranges: Vec<(Style, &str)> = h.highlight_line(line, &ps).unwrap();
-        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
-        ui.set_editor_text(escaped.to_string().into());
-        //print!("{}", escaped);
-    }
 
     let ui_handle = ui.as_weak();
     ui.on_request_execute(move || {
